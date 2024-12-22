1d3aef6606abf003ca63215ba8d22aaedb4f9003
amityadav86 <amitlko1986@gmail.com>
2024-12-22 18:34:52 +0530

    Pdf file Upload feature

--- version.php
+++ version.php
@@ -18,7 +18,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'plagiarism_origai';
-$plugin->version =  2024121400;
+$plugin->version =  2024122200;
 $plugin->requires = 2020061500;
 $plugin->release   = '1.1.3';
 $plugin->maturity  = MATURITY_STABLE;
